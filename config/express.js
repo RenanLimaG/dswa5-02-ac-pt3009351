7154a370d1dbed90d0253e01d840240d371667e2
RenanLimaG <renanlimag10@gmail.com>
2022-02-28 23:07:09 -0300

    Commit inicial

--- config/express.js
+++ config/express.js
@@ -1,13 +1,20 @@
 var express = require('express');
-var home = require('../app/routes/home');
+var bodyParser = require('body-parser');
+var load = require('express-load');
 module.exports = function(){
 	var app = express();
 	//Mudança no parâmetro PORT para publicar no Heroku
 	app.set('port', process.env.PORT);
 	//Middleware
 	app.use(express.static('./public'));
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
+
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
-	home(app);
+	
+	//Carregar pastas
+    load('models', {cwd: 'app'}).then('controllers').then('routes').into(app);
 	return app;
 };
